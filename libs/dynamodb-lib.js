3926dfcec168a30a8bc3936d76355cbdcf7b10fc
Daniel Curtis <Daniel.Curtis@ipreo.com>
2019-03-12 16:25:49 -0400

    removed old files

--- libs/dynamodb-lib.js
+++
@@ -1,8 +0,0 @@
-import AWS from "aws-sdk";
-
-export function call(action, params){
-    const dynamoDb = new AWS.DynamoDB.DocumentClient();
-    // if your table is in another region than your lambda
-    //AWS.config.update({ region: "my-region" });
-    return dynamoDB[action](params).promise();
-}
