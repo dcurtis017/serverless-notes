ca162fa1cb558d653d195daa8836e98743fdff02
Daniel Curtis <Daniel.Curtis@ipreo.com>
2019-03-13 15:14:09 -0400

    added dynamodb resource changed client homepage

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { API } from "aws-amplify";
 import "./Home.css";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   constructor(props) {
@@ -36,23 +37,23 @@ export default class Home extends Component {
       (note, i) =>
         i !== 0
           ? <LinkContainer
-              key={note.noteId}
-              to={`/notes/${note.noteId}`}
-            >
-              <ListGroupItem header={note.content.trim().split("\n")[0]}>
-                {"Created: " + new Date(note.createdAt).toLocaleString()}
-              </ListGroupItem>
-            </LinkContainer>
+            key={note.noteId}
+            to={`/notes/${note.noteId}`}
+          >
+            <ListGroupItem header={note.content.trim().split("\n")[0]}>
+              {"Created: " + new Date(note.createdAt).toLocaleString()}
+            </ListGroupItem>
+          </LinkContainer>
           : <LinkContainer
-              key="new"
-              to="/notes/new"
-            >
-              <ListGroupItem>
-                <h4>
-                  <b>{"\uFF0B"}</b> Create a new note
+            key="new"
+            to="/notes/new"
+          >
+            <ListGroupItem>
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new note
                 </h4>
-              </ListGroupItem>
-            </LinkContainer>
+            </ListGroupItem>
+          </LinkContainer>
     );
   }
 
@@ -61,6 +62,10 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple not taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">Login</Link>
+          <Link to="/signup" classname="btn btn-success btn-lg">Signup</Link>
+        </div>
       </div>
     );
   }
